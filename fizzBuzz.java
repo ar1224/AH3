bcd4eface2b014bba44827982761ceab2adb791d
Ramroop <alyssa.ramroop@fiserv.com>
2023-10-16 16:13:39 -0400

    ahhh3

--- fizzBuzz.java
+++ fizzBuzz.java
@@ -2,21 +2,21 @@ package AH3;
 import java.util.Scanner;
 
 public class fizzBuzz {
+
     public static void main(String[] args){
+        Scanner console = new Scanner(System.in);
+        int num;
+        System.out.println("Enter a max number..");
+        num = console.nextInt();
 
-    Scanner console = new Scanner(System.in);
-    int num;
-    System.out.println("Enter the max number..");
-    num = console.nextInt();
-
-    for (int i=1; i<num; i++){
-        if (i%3==0)
-            System.out.print("Fizz");
-        if (i%5==0)
-            System.out.println("Buzz");
-        if (i%3==0 && i%5==0)
-            System.out.println("FizzBuzz");
-        System.out.print(i);
-        }
+        for (int i=1; i<=num; i++){
+            String string = "";
+            if(i%3==0)string+="Fizz";
+            if(i%5==0)string+="Buzz";
+            if(string=="")string+=i;
+            System.out.print(string + " ");
+            }
+            
     }
 }
+
